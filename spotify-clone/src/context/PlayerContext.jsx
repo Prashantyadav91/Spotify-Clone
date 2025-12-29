04e1fa5c5457ba960fb162259519f1818f9e848d
Prashant Yadav <154693743+Prashantyadav91@users.noreply.github.com>
2025-12-29 23:08:42 +0530

    Update PlayerContext.jsx

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -9,7 +9,7 @@ const PlayerContextProvider = (props) => {
     const seekBg = useRef()
     const seekBar = useRef()
 
-    const url = "http://localhost:4000";
+    const url = "https://spotify-clone-backend-j3bh.onrender.com";
     const [songsData, setSongsData] = useState([])
     const [albumsData, setAlbumsData] = useState([])
 
